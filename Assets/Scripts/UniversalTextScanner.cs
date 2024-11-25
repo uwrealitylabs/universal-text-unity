ec9f2c40c50bd69cd3c70df921ad8a15360408f7
Joshua Zheng <jzheng796@gmail.com>
2024-11-25 06:47:16 -0500

    Implemented grab SearchPoint functionality and minor changes to SearchPoint class

--- Assets/Scripts/UniversalTextScanner.cs
+++ Assets/Scripts/UniversalTextScanner.cs
@@ -1,11 +1,20 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using Oculus.Interaction;
+using Oculus.Interaction.HandGrab;
+using Oculus.Interaction.GrabAPI;
+using Oculus.Interaction.Input;
+using Oculus.Interaction.Throw;
+
 public class UniversalTextScanner : MonoBehaviour
 {
-    //to-do: impl. methods of search for different search points
+    //to-do: impl. point, nearby, looking at
+    public HandGrabInteractor handGrabInteractor;
+    public HandGrabInteractor handGrabInteractorRight;
+
     public class SearchPoint : StringAggregator
     {
         private string _description; //desc of search point, e.g. "The user is pointing at"
@@ -22,27 +31,63 @@ public class UniversalTextScanner : MonoBehaviour
         protected override string Aggregate()
         {
             List<UniversalTextTag> tags = Search();
-            string representation = "";
+            string representation = _description + " ";
+            if (tags.Count == 0)
+            {
+                return "";
+            }
+            else if (tags.Count == 1)
+            {
+                return representation + tags[0].ToString();
+            }
             int index = 0;
             foreach (UniversalTextTag tag in tags)
             {
-                tagRep = tag.Representation;
-                if (index > 0)
+                string tagRep = tag.ToString();
+                if (tag == tags.Last())
                 {
-                    representation += "Also, " + char.ToLower(_description[0]) + _description.Substring(1);
-                }
-                else
+                    representation += " and" + tagRep + ".";
+                } else
                 {
-                    representation += _description;
+                    representation += ", " + tagRep;
                 }
-                representation += char.ToLower(tagRep[0]) + tagRep.Substring(1) + " ";
                 index++;
             }
             /*
-             * ex {"An apple on top of a plate.", "A pencil on the table"} => "The user is pointing at an apple on top of a plate. Also, 
-             * the user is pointing at a pencil on the table, which is 60 cm by 90 cm."
+             * ex {"an apple on top of a plate", "a pencil on the table"} => "The user is pointing at an apple on top of a plate and
+             * a pencil on the table, which is 60 cm by 90 cm."
              */
             return representation;
+        }
+    }
+
+    public List<UniversalTextTag> getGrabbedObjects()
+    {
+        List<UniversalTextTag> tags = new List<UniversalTextTag>();
+        if (handGrabInteractor.HasSelectedInteractable)
+        {
+            HandGrabInteractable grabbedObject = handGrabInteractor.SelectedInteractable;
+            GameObject grabbedGameObject = grabbedObject.gameObject;
+            UniversalTextTag tag = grabbedGameObject.GetComponent<UniversalTextTag>();
+            tags.Add(tag);
+        }
+        else if (handGrabInteractorRight.HasSelectedInteractable)
+        {
+            HandGrabInteractable grabbedObject = handGrabInteractorRight.SelectedInteractable;
+            GameObject grabbedGameObject = grabbedObject.gameObject;
+            UniversalTextTag tag = grabbedGameObject.GetComponent<UniversalTextTag>();
+            tags.Add(tag);
+        }
+        return tags;
+    }
+
+    private void Update()
+    {
+        //grabbing SearchPoint
+        SearchPoint grab = new SearchPoint("The user is grabbing", getGrabbedObjects);
+        if (grab.Search().Count > 0)
+        {
+            Debug.Log(grab.ToString());
         }
     }
 }
