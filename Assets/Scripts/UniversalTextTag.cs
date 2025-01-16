97fa8fb159cd356988740a16a374951b7c852e9c
nathanrun1 <nathanmreilly@gmail.com>
2025-01-16 11:41:10 -0500

    Cleaned up UTT

--- Assets/Scripts/UniversalTextTag.cs
+++ Assets/Scripts/UniversalTextTag.cs
@@ -1,16 +1,10 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
-using UnityEditor;
-using Unity.VisualScripting;
 using System.Linq;
-using Meta.XR.ImmersiveDebugger.UserInterface;
-using Meta.XR.Editor.Tags;
 
 /// <summary>
-/// A tag that marks this GameObject to be included in the overall real-time text representation
+/// A tag that marks this GameObject to be recognized by the UTS and thus included in the RTR
 /// </summary>
 public class UniversalTextTag : MonoBehaviour
 {
@@ -27,19 +21,26 @@ public class UniversalTextTag : MonoBehaviour
     public override string ToString()
     {
         string representation = $"{String.Copy(Description)}";
-        if (Attributes.Count == 0)
+
+        // Clean out invalid attributes
+        List<Attribute> cleanedAttributes = new List<Attribute>();
+        foreach (Attribute attribute in Attributes)
+        {
+            if (attribute.Valid) cleanedAttributes.Add(attribute);
+        }
+
+        if (cleanedAttributes.Count == 0)
         {
             return representation += ".";
         }
-        representation += ", which is ";
-        if (Attributes.Count == 1)
+        representation += ", which ";
+        if (cleanedAttributes.Count == 1)
         {
-            return representation += $"{Attributes.First()}.";
+            return representation += $"{cleanedAttributes.First()}.";
         }
-        foreach (Attribute attribute in Attributes)
+        foreach (Attribute attribute in cleanedAttributes)
         {
-            
-            if (attribute == Attributes.Last())
+            if (attribute == cleanedAttributes.Last())
             {
                 representation += $"and {attribute}.";
             } else
@@ -54,7 +55,7 @@ public class UniversalTextTag : MonoBehaviour
     /// Describes an attribute of this UniversalTextTag's Object. Attribute values can either be initialized with a value type or
     /// a function that returns the current value of the attribute when called.
     /// </summary>
-    public class Attribute : StringAggregator
+    public class Attribute
     {
         /// <summary>
         /// Description of the attribute
@@ -74,12 +75,12 @@ public class UniversalTextTag : MonoBehaviour
         /// <summary>
         /// Current value of this Attribute
         /// </summary>
-        protected object? Value { get => _valueGetter(); }
+        protected object Value { get => _valueGetter(); }
 
         /// <summary>
         /// Validity of this Attribute (true if relevant to the RTR, false otherwise)
         /// </summary>
-        protected bool Valid { get => _validGetter(); }
+        public bool Valid { get => _validGetter(); }
 
         /// <summary>
         /// Initializes an Attribute with provided 'formattedDescription', and delegates referencing getter functions for 
@@ -88,54 +89,18 @@ public class UniversalTextTag : MonoBehaviour
         /// <param name="formattedDescription">Formatted description of the Attribute, must be compatible with String.Format(formattedDescription, valueGetter()) when Value is not null</param>
         /// <param name="valueGetter">Reference to getter function that retrieves the associated attribute's current value</param>
         /// <param name="validGetter">Reference to getter function that returns a bool value representing whether or not this Attribute is currently relevant to the RTR</param>
-        public Attribute(string formattedDescription, Func<object> valueGetter, Func<bool> validGetter)
+        public Attribute(string formattedDescription, Func<object> valueGetter = null, Func<bool> validGetter = null)
         {
             _description = formattedDescription;
-            _valueGetter = valueGetter;
-            _validGetter = validGetter;
+
+            if (valueGetter != null) _valueGetter = valueGetter;
+            else _valueGetter = () => null;
+
+            if (validGetter != null) _validGetter = validGetter;
+            else _validGetter = () => true;
         }
 
-        /// <summary>
-        /// Initializes an Attribute with provided 'formattedDescription', and delegate referencing a getter function for 
-        /// the Attribute's current value. Attributes initialized this way are assumed to always be relevant to the RTR.
-        /// </summary>
-        /// <param name="formattedDescription">Formatted description of the Attribute, must be compatible with String.Format(formattedDescription, valueGetter()) when Value is not null</param>
-        /// <param name="valueGetter">Reference to getter function that retrieves the associated attribute's current value</param>
-        public Attribute(string formattedDescription, Func<object> valueGetter)
-        {
-            _description = formattedDescription;
-            _valueGetter = valueGetter;
-            _validGetter = () => true;
-        }
-
-        /// <summary>
-        /// Initializes an Attribute with provided string 'description', and delegate referencing getter function for the Attribute's current validity.
-        /// Attributes initialized this way are not linked to a value outside of the object, and thus the provided 'description' must describe the attribute 
-        /// entirely, including its value, since the description cannot be changed.
-        /// </summary>
-        /// <param name="description">Description of the Attribute</param>
-        /// <param name="validGetter">Reference to getter function that returns a bool value representing whether or not this Attribute is currently relevant to the RTR</param>
-        public Attribute(string description, Func<bool> validGetter)
-        {
-            _description = description;
-            _valueGetter = () => null;
-            _validGetter = validGetter;
-        }
-
-        /// <summary>
-        /// Initializes an Attribute with provided string 'description'. Attributes initialized this way are not linked to a value outside of the object, 
-        /// and thus the provided 'description' must describe the attribute entirely, including its value, since the description cannot be changed.
-        /// Additionally, Attributes initialized this way are assumed to always be relevant to the RTR.
-        /// </summary>
-        /// <param name="description">Description of the Attribute</param>
-        public Attribute(string description)
-        {
-            _description = description;
-            _valueGetter = () => null;
-            _validGetter = () => true;
-        }
-
-        protected override string Aggregate()
+        public override string ToString()
         {
             if (Value == null)
             {
@@ -170,54 +135,13 @@ public class UniversalTextTag : MonoBehaviour
         /// <param name="tagsGetter">Reference to getter function that returns a list of the UniversalTextTags that this relation is currently held with</param>
         /// <param name="validGetter">Reference to getter function that returns a bool value representing whether or not this Attribute is currently relevant to the RTR</param>
         /// <param name="valueGetter">Reference to getter function that retrieves the associated attribute's current value</param>
-        public Relation(string description, Func<List<UniversalTextTag>> tagsGetter, Func<bool> validGetter, Func<object> valueGetter)
+        public Relation(string description, Func<List<UniversalTextTag>> tagsGetter, Func<bool> validGetter = null, Func<object> valueGetter = null)
             : base(description, valueGetter, validGetter)
         {
             _tagsGetter = tagsGetter;
         }
 
-        /// <summary>
-        /// Initialize a Relation with provided format string 'formattedDescription', and delegates referencing getter functions for
-        /// a List of UniversalTextTags that the relation is held with and the validity of this relation.
-        /// </summary>
-        /// <param name="formattedDescription">Formatted description of the Attribute, must be compatible with String.Format(formattedDescription, tagsString) where
-        /// tagsString is the UTTs listed in natural language</param>
-        /// <param name="tagsGetter">Reference to getter function that returns a list of the UniversalTextTags that this relation is currently held with</param>
-        /// <param name="validGetter">Reference to getter function that returns a bool value representing whether or not this Attribute is currently relevant to the RTR</param>
-        public Relation(string description, Func<List<UniversalTextTag>> tagsGetter, Func<bool> validGetter)
-            : base(description, validGetter)
-        {
-            _tagsGetter = tagsGetter;
-        }
-
-        /// <summary>
-        /// Initialize a Relation with provided format string 'formattedDescription', and delegates referencing getter functions for
-        /// a List of UniversalTextTags that the relation is held with and the value of this relation.
-        /// </summary>
-        /// <param name="formattedDescription">Formatted description of the Attribute, must be compatible with String.Format(formattedDescription, valueGetter(), tagsString) when Value is not null, where
-        /// tagsString is the UTTs listed in natural language</param>
-        /// <param name="tagsGetter">Reference to getter function that returns a list of the UniversalTextTags that this relation is currently held with</param>
-        /// <param name="valueGetter">Reference to getter function that retrieves the associated attribute's current value</param>
-        public Relation(string description, Func<List<UniversalTextTag>> tagsGetter, Func<object> valueGetter)
-            : base(description, valueGetter)
-        {
-            _tagsGetter = tagsGetter;
-        }
-
-        /// <summary>
-        /// Initialize a Relation with provided format string 'formattedDescription', and delegates referencing a getter function for
-        /// a List of UniversalTextTags that the relation is held with.
-        /// </summary>
-        /// <param name="formattedDescription">Formatted description of the Attribute, must be compatible with String.Format(formattedDescription, tagsString) where
-        /// tagsString is the UTTs listed in natural language</param>
-        /// <param name="tagsGetter">Reference to getter function that returns a list of the UniversalTextTags that this relation is currently held with</param>
-        public Relation(string description, Func<List<UniversalTextTag>> tagsGetter)
-            : base(description)
-        {
-            _tagsGetter = tagsGetter;
-        }
-
-        protected override string Aggregate()
+        public override string ToString()
         {
             string tagsString = "";
             if (Tags.Count == 1) { tagsString = Tags.First().ToString(); }
