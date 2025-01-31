ebffc96fd037f2d37fb2ed1e4bd0effb9306f6b6
joshua-z-zheng <jzheng796@gmail.com>
2025-01-31 03:23:53 -0500

    added looking at SearchPoint

--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -8,11 +8,14 @@ public class Test : MonoBehaviour
 {
     public HandGrabInteractor leftHandGrabInteractor;
     public HandGrabInteractor rightHandGrabInteractor;
+    public Camera xrCamera;
+    public float maxViewDistance = 10f;
     
 
     void Start()
     {
         UniversalTextScanner.Instance.AddSearchPoint(new GrabbingSearchPoint(leftHandGrabInteractor, rightHandGrabInteractor));
+        UniversalTextScanner.Instance.AddSearchPoint(new LookingSearchPoint(xrCamera, maxViewDistance));
         UniversalTextScanner.Instance.Generate();
         StartCoroutine(PrintUTS());
     }
